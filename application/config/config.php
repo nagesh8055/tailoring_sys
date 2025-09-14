11273a4414a3c33cf1dd57b1924618ae31d4f521
Shrikant Ratnaparkhi <shrikant008@gmail.com>
2025-09-14 11:41:42 +0530

    updated base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://bktailors.avighnatech.co.in/';
+$config['base_url'] = 'https://tailor.avighnatech.co.in';
 
 /*
 |--------------------------------------------------------------------------
